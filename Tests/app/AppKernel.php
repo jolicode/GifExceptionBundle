674175af85312de8a7a140d5c7e982619742744e
Loïck Piera <pyrech@gmail.com>
2016-02-26 14:18:41 +0100

    Simplify tests kernel config :kissing_cat:

--- Tests/app/AppKernel.php
+++ Tests/app/AppKernel.php
@@ -9,6 +9,7 @@
 namespace Joli\GifExceptionBundle\Tests\app;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
@@ -35,12 +36,15 @@ class AppKernel extends Kernel
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        if (version_compare(self::VERSION, '2.7.0', '>=')) {
-            $loader->load($this->getRootDir() . '/config/config_27.yml');
-
-            return;
-        }
-
         $loader->load($this->getRootDir() . '/config/config.yml');
+
+        // graciously stolen from https://github.com/javiereguiluz/EasyAdminBundle/blob/master/Tests/Fixtures/App/AppKernel.php#L39-L45
+        if (3 === static::MAJOR_VERSION) {
+            $loader->load(function (ContainerBuilder $container) {
+                $container->loadFromExtension('framework', array(
+                    'assets' => null,
+                ));
+            });
+        }
     }
 }
