c17e1a7bd9303db67d5f6eaff4797304234f4d14
Ian Jenkins <ian.jenkins@boxuk.com>
2016-10-03 15:17:36 +0100

    Trying with a lower optimization level

--- Command/GifOptimizerCommand.php
+++ Command/GifOptimizerCommand.php
@@ -26,7 +26,7 @@ class GifOptimizerCommand extends Command
     /**
      * @var string
      */
-    const DEFAULT_OPTIMIZATION_LEVEL = '-O3';
+    const DEFAULT_OPTIMIZATION_LEVEL = '-O1';
 
     /**
      * @var int
